12b4272eb5f8a7d085cb6d841034789d1102cfa7
chase <chasewhip20@gmail.com>
2020-01-15 19:44:00 -0700

    Added more stuffs

--- src/main/java/SeedReverse.java
+++ src/main/java/SeedReverse.java
@@ -1,7 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SeedReverse {
+    private ArrayList<Thread> threads;
+
     public static void main(String[] args) {
         new SeedReverse();
     }
@@ -16,7 +21,9 @@ public class SeedReverse {
         System.out.println("Check X Distance: ");
         final int CHECK_X_DIST = scanner.nextInt();
 
-        ArrayList<Thread> threads = new ArrayList<Thread>();
+        scanner.close();
+
+        threads = new ArrayList<Thread>();
 
         long segment = 1L << 48 / NUM_THREADS;
         long startSeed = 0;
@@ -27,7 +34,8 @@ public class SeedReverse {
                     startSeed + (segment * x),
                     startSeed + (segment * (x + 1)),
                     CHECK_SEED,
-                    CHECK_X_DIST)
+                    CHECK_X_DIST,
+                    this)
             );
 
             threads.add(thread);
@@ -36,19 +44,35 @@ public class SeedReverse {
         System.out.println("Starting threads...");
 
         for (Thread thread : threads){
-            System.out.println("Starting thread.");
             thread.start();
         }
 
-        System.out.println("Loaded All threads.");
+        System.out.println("Started threads");
 
         while (true){
             try {
                 Thread.sleep(10000);
-                System.out.println("Searching... (Main Thread)");
             } catch (InterruptedException e){
                 e.printStackTrace();
             }
+        }
+    }
+
+    synchronized void stopThreads(){
+        for (Thread thread : threads){
+            thread.stop();  //yes i know its bad
+        }
+        System.out.println("All threads stopped.");
+    }
+
+    synchronized void foundSeed(long seed){
+        File file = new File("output.txt");
+        file.mkdirs();
+        try {
+            PrintWriter out = new PrintWriter(file);
+            out.println(seed);
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/WorkerRunner.java
+++ src/main/java/WorkerRunner.java
@@ -1,33 +1,49 @@
 import java.util.Random;
 
 public class WorkerRunner implements Runnable{
+    private SeedReverse instance;
+
     private long startSeed;
     private long endSeed;
     private long checkSeed;
     private int checkXDist;
 
-    public WorkerRunner(long startSeed, long endSeed, long checkSeed, int checkXDist) {
+    public WorkerRunner(long startSeed, long endSeed, long checkSeed, int checkXDist, SeedReverse instance) {
         this.startSeed = startSeed;
         this.endSeed = endSeed;
         this.checkSeed = checkSeed;
         this.checkXDist = checkXDist;
+        this.instance = instance;
     }
 
     public void run() {
         final Random rand = new Random();
 
         for (long randomSeed = startSeed; randomSeed < endSeed; randomSeed++) {
+            System.out.println("test");
+
             rand.setSeed(randomSeed);
             final long a = rand.nextLong() | 1L;
             for (int x = -checkXDist; x <= checkXDist; x++) {
                 if (((long) x * a ^ randomSeed) == checkSeed) {
-                    System.out.println(
-                        "Found Seed Match" +
-                            "\nSeed: " + randomSeed +
-                            "\nChunk Seed: " + checkSeed
-                    );
+                    long chunkSeed = ((long) x * a ^ randomSeed);
+                    System.out.println("Found possible match (ChunkSeed): " + chunkSeed);
+                    if (validSeed(chunkSeed)) {
+                        System.out.println(
+                            "Found Seed Match" +
+                                "\nSeed: " + randomSeed +
+                                "\nChunk Seed: " + checkSeed
+                        );
+
+                        instance.foundSeed(randomSeed);
+                        instance.stopThreads();
+                    } else {
+                        System.out.println("Chunk seed was invalid, skipping.");
+                    }
                 }
             }
         }
     }
+
+    private static boolean validSeed(long a){long b=18218081,c=1L<<48,d=7847617,e=((((d*((24667315*(a>>>32)+b*(int)a+67552711)>>32)-b*((-4824621*(a>>>32)+d*(int)a+d)>>32))-11)*0xdfe05bcb1365L)%c);return((((0x5deece66dl*e+11)%c)>>16)<<32)+(int)(((0xbb20b4600a69L*e+0x40942de6baL)%c)>>16)==a;}
 }
